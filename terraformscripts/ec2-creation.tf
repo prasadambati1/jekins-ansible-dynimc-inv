49492790334a0f0166b627bb2e3dcf850a0ed133
prasadambati1 <139728981+prasadambati1@users.noreply.github.com>
2023-09-08 07:51:34 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,17 +1,12 @@
-
 provider "aws"{
-region = "ap-south-1"
+    region = "ap-south-1"
 }
-
 resource "aws_instance" "AWSServer"{
-ami = "ami-0da59f1af71ea4ad2"
-instance_type = "t2.micro"
-key_name = "devopsmithuntechnologiesbhaskar"
-security_groups = ["launch-wizard-2"]
+    ami = "ami-02bb7d8191b50f4bb"
+    instance_type = "t2.micro"
+    security_groups = ["launch-wizard-11"]
+    key_name = "devopsprasad"
+}
 tags = {
-Name = "tomcatsservers"
+    Name = "tomcatservers"
 }
-}
-
-
-
